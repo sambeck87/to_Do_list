860cd3594be7fe80d079f35855909b7d959cf3b4
sambeck87 <sandro_mipb@hotmail.com>
2022-12-08 09:12:01 -0600

    Fix linter errors

--- src/modules/setup.js
+++ src/modules/setup.js
@@ -1,19 +1,9 @@
-/*export default function setup() {
-  let allTasks = localStorage.getItem('tasks');
-  allTasks = JSON.parse(allTasks);
-  for (let i = 0; i < allTasks.length; i += 1) {
-    allTasks[i].complete = false;
-  }
-  allTasks = JSON.stringify(allTasks);
-  localStorage.setItem('tasks', allTasks);
-}*/
-
 export default function setup() {
   let allTasks = localStorage.getItem('tasks');
   allTasks = JSON.parse(allTasks);
   for (let i = 0; i < allTasks.length; i += 1) {
     const num = allTasks[i].index;
-    if(allTasks[i].complete === true){
+    if (allTasks[i].complete === true) {
       const text = document.getElementById(i);
       text.classList.add('cross_out');
       const box = document.getElementById(`task${num}`);
