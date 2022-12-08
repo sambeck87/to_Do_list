6d0b5a96683f4d5b3e174d4ea12fc13a19465b08
sambeck87 <sandro_mipb@hotmail.com>
2022-12-08 09:07:52 -0600

    Change README file Add new function that save the checkbox status

--- src/modules/setup.js
+++ src/modules/setup.js
@@ -1,4 +1,4 @@
-export default function setup() {
+/*export default function setup() {
   let allTasks = localStorage.getItem('tasks');
   allTasks = JSON.parse(allTasks);
   for (let i = 0; i < allTasks.length; i += 1) {
@@ -6,4 +6,18 @@ export default function setup() {
   }
   allTasks = JSON.stringify(allTasks);
   localStorage.setItem('tasks', allTasks);
+}*/
+
+export default function setup() {
+  let allTasks = localStorage.getItem('tasks');
+  allTasks = JSON.parse(allTasks);
+  for (let i = 0; i < allTasks.length; i += 1) {
+    const num = allTasks[i].index;
+    if(allTasks[i].complete === true){
+      const text = document.getElementById(i);
+      text.classList.add('cross_out');
+      const box = document.getElementById(`task${num}`);
+      box.checked = true;
+    }
+  }
 }
